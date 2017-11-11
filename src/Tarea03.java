39795d8c0052240ff9b3336148e5553d5d947836
Amarico <Amarico@192.168.56.1>
2017-11-11 13:50:05 +0100

    He creado un nuevo paquete llamado tarea03
    A continuación hemos creado un enumerado llamado Color que contiene los
    valores BLANCO Y NEGRO

--- src/Tarea03.java
+++ src/Tarea03.java
@@ -1,8 +1,16 @@
 
 public class Tarea03 {
 
+	// Creamos el enumerado Color con los valores BLANCO Y NEGRO
+	public enum Color {
+		BLANCO(),
+		NEGRO();
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
 
 	}
 
