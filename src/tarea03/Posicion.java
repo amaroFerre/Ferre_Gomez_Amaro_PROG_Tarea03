62b775018af58fcce8e67a61fc4e3ce45b8f19fb
Amarico <Amarico@192.168.56.1>
2017-11-11 15:03:56 +0100

    Creo la clase posicion y pongo como private los atributos fila como entero (int)y columna como caracter (char)

---
+++ src/tarea03/Posicion.java
@@ -0,0 +1,15 @@
+package tarea03;
+
+public class Posicion {
+	// Creo los atributos fila y columna como private
+	private int fila;
+	
+	private char columna; 
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
